571dad4896be324459ea5e5d41d6fccc0ca2d288
seebirdfly <137145863+seebirdfly@users.noreply.github.com>
2023-06-20 14:18:21 +0800

    Create RunAsync.js

---
+++ RunAsync.js
@@ -0,0 +1,25 @@
+const runAsync = (fn) => {
+  const worker = new Worker(
+    URL.createObjectURL(new Blob([`postMessage((${fn})());`]), {
+      type: "application/javascript; charset=utf-8",
+    })
+  );
+  return new Promise((res, rej) => {
+    worker.onmessage = ({ data }) => {
+      res(data), worker.terminate();
+    };
+    worker.onerror = (err) => {
+      rej(err), worker.terminate();
+    };
+  });
+};
+
+// Examples
+const longRunningFunction = () => {
+  let result = 0;
+  for (let i = 0; i < 1000; i++)
+    for (let j = 0; j < 700; j++)
+      for (let k = 0; k < 300; k++) result = result + i + j + k;
+
+  return result;
+};
